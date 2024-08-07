fef43de0fe3a03847d33dc566489a1ace878fa74
MohamedAmr23 <amrm96754@gmail.com>
2024-08-07 06:17:33 +0300

    string-repeat

---
+++ String-repeat.js
@@ -0,0 +1,11 @@
+// Code wars Kata8 Solutions
+
+// Create Empty String
+// Take the Number Argument And Loop through it
+// Add the String to the Empty one
+
+function repeatStr (n, s) {
+  
+    return s.repeat(n);
+  }
+  console.log( repeatStr(6,"hello"))
