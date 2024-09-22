56ad6b7677e9fcc88d36b38c4516fae329b26f1b
MohamedAmr23 <amrm96754@gmail.com>
2024-09-22 19:00:29 +0300

    Convert-boolean-values-to-strings-'Yes'-or-'No' .js

---
+++ Convert-boolean-values-to-strings-'Yes'-or-'No'.js
@@ -0,0 +1,10 @@
+function boolToWord( bool ){
+    // if(bool==true){
+    //     console.log('Yes')
+    // }else if(bool==false){
+    //     console.log('No')
+    // }
+    bool?'Yes':'No'
+  }
+
+ console.log(boolToWord(true))
