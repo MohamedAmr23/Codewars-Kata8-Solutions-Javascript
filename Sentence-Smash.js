98d16cb0da985b925d8d4f7dd047feec54051587
MohamedAmr23 <amrm96754@gmail.com>
2024-10-02 03:37:01 +0300

    Sentence Smash

---
+++ Sentence-Smash.js
@@ -0,0 +1,9 @@
+function smash (words) {
+    return words.join(' ')
+ };
+console.log(smash(['hello', 'world', 'this', 'is', 'great']))
+// let arr=['hello', 'world', 'this', 'is', 'great']
+
+// let word = arr.join(' ')
+
+// console.log(word)
