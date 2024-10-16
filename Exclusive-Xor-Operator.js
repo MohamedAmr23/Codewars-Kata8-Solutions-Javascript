e472ac6c6c2d6b07ddb008bdc5ef912bfa75b50b
MohamedAmr23 <amrm96754@gmail.com>
2024-10-16 20:36:53 +0300

    Exclusive Xor Operator

---
+++ Exclusive-Xor-Operator.js
@@ -0,0 +1,18 @@
+// function xor(a, b) {
+//     if(a===false && b===false ){
+//         return false
+//     }else if (a===true && b===false){
+//         return true
+//     }else if (a===false && b===true){
+//         return true
+//     }else{
+//         return false
+//     }
+//   }
+
+//   console.log(xor(false, false))
+// anohter solution
+function xor(a, b) {
+    return a === b ? false : true;
+  }
+  console.log(xor(false, false));
